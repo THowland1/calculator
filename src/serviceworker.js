f0a7e3a8546a10cc48ee66ea63f55d9e9a8a34df
Tom Howland <tomhowland9@gmail.com>
2022-07-23 20:57:27 +0100

    Bust service worker cache

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,41 +1,48 @@
 import { manifest, version } from '@parcel/service-worker';
 
 const OFFLINE_URL = 'index.html';
+const version = process.env.BUILD_TIME;
 
 self.addEventListener('install', function (event) {
-    console.log('[ServiceWorker] Install');
+  console.log('[ServiceWorker] Install');
 
-    event.waitUntil((async () => {
-        const cache = await caches.open(version);
-        // Setting {cache: 'reload'} in the new request will ensure that the response
-        // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-        await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
-        await cache.add('/');
-        await cache.addAll(manifest);
-    })());
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(version);
+      // Setting {cache: 'reload'} in the new request will ensure that the response
+      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+      await cache.add('/');
+      await cache.addAll(manifest);
+    })()
+  );
 
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[ServiceWorker] Activate');
-    event.waitUntil((async () => {
-        // Enable navigation preload if it's supported.
-        // See https://developers.google.com/web/updates/2017/02/navigation-preload
-        if ('navigationPreload' in self.registration) {
-            await self.registration.navigationPreload.enable();
-        }
-    })());
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ('navigationPreload' in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
 
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', function (event) {
-    // console.log('[Service Worker] Fetch', event.request.url);
-    event.respondWith((async () => {
-        const cachedResponse = await caches.match(event.request);
-        console.log;
-        return cachedResponse || fetch(event.request);
-    })());
+  // console.log('[Service Worker] Fetch', event.request.url);
+  event.respondWith(
+    (async () => {
+      const cachedResponse = await caches.match(event.request);
+      console.log;
+      return cachedResponse || fetch(event.request);
+    })()
+  );
 });
